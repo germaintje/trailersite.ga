b3a1a1c80f10f7f17488db90c53819bbf8ced793
Chen pei Hu <chenpeihu6@gmail.com>
2020-05-15 22:27:42 +0200

    homepage responsive

--- model/MovieApiLogic.php
+++ model/MovieApiLogic.php
@@ -73,7 +73,7 @@ class MovieApiLogic
                     $imageurl = $content["poster_path"];
                     $videospider_url = file_get_contents("https://vsrequest.video/request.php?key=DBUBFDLOJCRjoGBA&secret_key=nzgbf338ysoh17zbrida1f4xrvt74d&video_id=$movie_id&tmdb=1&ip=$ip");
 
-                    $html .= "<div class='movie-poster-box col-3'>";
+                    $html .= "<div class='movie-poster-box col-12 col-sm-6 col-md-4 col-lg-3'>";
                     $html .= "<a href='?request=watch&link=$videospider_url&mov_id=$movie_id' title='$movie_title' class='home-movie-link'>";
                     $html .= "<img src='http://image.tmdb.org/t/p/w185$imageurl' alt=''><br>";
                     $html .= "$movie_title</a>";
